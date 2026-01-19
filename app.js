a3ea8b6c3c5f3ba1ec63b993ab1265b9ec87e584
BhavaniBoyapati <bhavaniboyapatimbry@gmail.com>
2026-01-19 19:05:02 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,1 +1,11 @@
+const express = require("express");
+const app = express();
 
+const PORT = process.env.PORT || 5000;
+
+app.get("/orders", (req, res) => {
+  res.json([
+    { orderId: 101, product: "Laptop" },
+    { orderId: 102, product: "Phone" }
+  ]);
+});
