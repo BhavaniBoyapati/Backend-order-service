2529df0e950766fccd3333def71d7337d6fbe98b
BhavaniBoyapati <bhavaniboyapatimbry@gmail.com>
2026-02-11 20:22:33 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,19 +1,39 @@
 const express = require("express");
+
+const cors = require("cors");
+ 
 const app = express();
 
 const PORT = process.env.PORT || 5000;
+ 
+app.use(cors({
 
+  origin: "https://mbry.digital", // replace with real frontend domain
+
+}));
+ 
 app.get("/orders", (req, res) => {
+
   res.json([
+
     { orderId: 101, product: "Laptop" },
+
     { orderId: 102, product: "Phone" }
+
   ]);
-});
 
+});
+ 
 app.get("/health", (req, res) => {
+
   res.send("Order Service is healthy");
+
+});
+ 
+app.listen(PORT, "0.0.0.0", () => {
+
+  console.log(`Order Service running on port ${PORT}`);
+
 });
 
-app.listen(PORT, () =>
-  console.log(`Order Service running on port ${PORT}`)
-);
+ 
